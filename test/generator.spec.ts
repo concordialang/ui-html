402f04afa1eb1a239d32af3dd020bec88dc6d270
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2019-04-11 16:04:08 -0300

    Add test to Generator (WIP)

---
+++ test/generator.spec.ts
@@ -0,0 +1,52 @@
+// Precisa instalar:
+// npm i --save-dev jest @types/ts-jest memfs html-minifier
+//
+import { vol, fs } from 'memfs';
+import { minify } from 'html-minifier';
+import Generator from '../src/classes/generator';
+
+describe( 'Generator', () => {
+
+    const CURRENT_DIR: string = process.cwd();
+
+    let generator: Generator;
+
+    beforeEach( () => {
+       vol.mkdirpSync( CURRENT_DIR, { recursive: true } ); // Synchronize with the current fs structure
+       generator = new Generator( fs ); // Cria com o fs em memória
+    } );
+
+    afterEach( () => {
+        generator = null;
+        vol.reset(); // Erase in-memory structure
+    } );
+
+    expectFeaturesProduceHtml( features: Feature[], htmls: string[] ): void {
+
+        const files: string[] = await generator.generate( features );
+        expect( files ).toHaveLength( htmls.length );
+
+        for ( let i in files ) {
+            const file: string = files[ i ];
+            const expectedHtml: string = htmls[ i ];
+            this.expectFileHasHtml( file, expectedHtml );
+        }
+    }
+
+    expectFileHasHtml( filePath: string, html: string ): void {
+        const expected = minify( html );
+        const produced = await fs.readFile( firstFile );
+        expected( minify( produced ) ).toEqual( expected );
+    }
+
+    it( 'produces an HTML file from a single button', async () => {
+        const features: Features = [  /* algo aqui */ ];
+        const htmls: string[] = [ minify(`
+        <html>
+            ... colocar aqui o que se espera gerar
+        </html>
+        `) ];
+        expectFeaturesProduceHtml( features, htmls );
+    } );
+
+} );
